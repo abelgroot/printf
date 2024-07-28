3b59c4a1f1905dec3a325faf40500aeecfdc81b6
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-28 14:05:14 +0300

    1-di_specifiers.c handled INT_MIN

--- 1-di_specifiers.c
+++ 1-di_specifiers.c
@@ -6,23 +6,32 @@
 * Return: number of characters printed.
 **/
 
-int print_number(long n)
+int print_number(int n)
 {
 	int printed_chars = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
 		printed_chars++;
+		if (n == INT_MIN)
+		{
+			n = -(n / 10);
+			printed_chars += print_number(n);
+			_putchar('8');
+			return (printed_chars + 1);
+		}
+		n = -n;
 	}
+
 	if (n / 10)
 		printed_chars += print_number(n / 10);
+
 	_putchar(n % 10 + '0');
 	printed_chars++;
+
 	return (printed_chars);
 }
-
 /**
 * d_specifier - function to print an integer.
 * @args: input argument from the function.
@@ -31,7 +40,7 @@ int print_number(long n)
 **/
 int d_specifier(va_list args)
 {
-	long n = va_arg(args, long);
+	int n = va_arg(args, int);
 
 	return (print_number(n));
 }
