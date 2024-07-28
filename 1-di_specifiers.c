e6728c6f2fbcb16fdecdb406b46c959ce5f4a95c
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-28 13:44:14 +0300

    1-di_specifiers.c removed int_min

--- 1-di_specifiers.c
+++ 1-di_specifiers.c
@@ -10,12 +10,6 @@ int print_number(long n)
 {
 	int printed_chars = 0;
 
-	if (n == INT_MIN)
-	{
-		printed_chars += _putchar('-');
-		printed_chars += _putchar(2);
-		n = 147483648;
-	}
 	if (n < 0)
 	{
 		_putchar('-');
