1265e4e45290ebe2fa607cdc734a4dc103d57d6b
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-29 08:43:25 +0300

    7_+space#_flag_specifiers.c refactored

--- 7_+space#_flag_specifiers.c
+++ 7_+space#_flag_specifiers.c
@@ -1,75 +1,137 @@
 #include "main.h"
 
 /**
-* plus_non_custom_specifier - Handles the '+' flag for non-custom specifiers.
-* @args: The argument list.
-*
-* Return: The number of characters printed.
-*/
-int plus_non_custom_specifier(va_list args)
+ * plus_non_custom_specifier - Handles the plus flag
+ * @args: The argument list
+ * @length_modifier: The length modifier
+ * @specifier: The format specifier
+ * Return: The number of characters printed
+ */
+int plus_non_custom_specifier(va_list args, char length_modifier, char specifier)
 {
-	int k = va_arg(args, int);
 	int printed_chars = 0;
 
-	if (k >= 0)
+	switch (specifier)
 	{
+		case 'd':
+		case 'i':
+			if (length_modifier == 'l')
+				printed_chars += plus_print_number(va_arg(args, long int));
+			else if (length_modifier == 'h')
+				printed_chars += plus_print_number((short) va_arg(args, int));
+			else
+				printed_chars += plus_print_number(va_arg(args, int));
+			break;
+		default:
+		 printed_chars += process_specifier(specifier, args, length_modifier);
+		break;
+	}
+return (printed_chars);
+}
+
+/**
+ * plus_print_number - Prints a number with a plus sign if positive
+ * @n: The number to print
+ * Return: The number of characters printed
+ */
+int plus_print_number(int n)
+{
+	if (n >= 0)
 		_putchar('+');
-		printed_chars += 1;
-		
-	}
-	printed_chars += print_number(k);
+	return (print_number(n) + (n >= 0 ? 1 : 0));
+}
 
+/**
+ * space_non_custom_specifier - Handles the space flag
+ * @args: The argument list
+ * @length_modifier: The length modifier
+ * @specifier: The format specifier
+ * Return: The number of characters printed
+ */
+int space_non_custom_specifier(va_list args, char length_modifier, char specifier)
+{
+	int printed_chars = 0;
+
+	switch (specifier)
+	{
+		case 'd':
+		case 'i':
+			if (length_modifier == 'l')
+				printed_chars += space_print_number(va_arg(args, long int));
+			else if (length_modifier == 'h')
+				printed_chars += space_print_number((short) va_arg(args, int));
+			else
+				printed_chars += space_print_number(va_arg(args, int));
+			break;
+		default:
+			printed_chars += process_specifier(specifier, args, length_modifier);
+			break;
+	}
 	return (printed_chars);
 }
 
 /**
-* space_non_custom_specifier - Handles the ' ' flag for non-custom specifiers.
-* @args: The argument list.
-*
-* Return: The number of characters printed.
+* space_print_number - Prints a number with a space if positive
+* @n: The number to print
+* Return: The number of characters printed
 */
-int space_non_custom_specifier(va_list args)
+int space_print_number(int n)
 {
-	int k = va_arg(args, int);
-	int printed_chars = 0;
-
-	if (k >= 0)
-	{
+	if (n >= 0)
 		_putchar(' ');
-		printed_chars += 1;
-	}
-	printed_chars += print_number(k);
-	return (printed_chars);
+	return (print_number(n) + (n >= 0 ? 1 : 0));
 }
 
-/**
-* hash_flag_specifier - Handles the '#' flag for 'o', 'x', and 'X' specifiers.
-* @args: The argument list.
-* @specifier: The specifier character.
-*
-* Return: The number of characters printed.
-*/
-int hash_flag_specifier(va_list args, char specifier)
-{
-	int printed_chars = 0;
+#include "main.h"
 
-	if (specifier == 'o')
+/**
+ * hash_flag_specifier - Handles the hash flag
+ * @args: The argument list
+ * @specifier: The format specifier
+ * @length_modifier: The length modifier
+ * Return: The number of characters printed
+ */
+int hash_flag_specifier(va_list args, char specifier, char length_modifier)
+{
+int printed_chars = 0;
+
+	switch (specifier)
 	{
-		_putchar('0');
-		printed_chars += 1;
-		printed_chars += o_specifier(args);
-	}
-	else if (specifier == 'x')
-	{
-		_puts("0x");
-		printed_chars += 2;
-		printed_chars += x_specifier(args);
-	}
-	else if (specifier == 'X')
-	{
-		_puts("0X");
-		printed_chars += 2;
-		printed_chars += X_specifier(args);
+		case 'o':
+			_putchar('0');
+			printed_chars++;
+			if (length_modifier == 'l')
+				printed_chars += lo_specifier(args);
+			else if (length_modifier == 'h')
+				printed_chars += ho_specifier(args);
+			else
+				printed_chars += convert_and_print(va_arg(args, unsigned int), 8, "01234567");
+			break;
+		case 'x':
+			_putchar('0');
+			_putchar('x');
+			printed_chars += 2;
+			if (length_modifier == 'l')
+				printed_chars += lx_specifier(args);
+			else if (length_modifier == 'h')
+				printed_chars += hx_specifier(args);
+			else
+				printed_chars += convert_and_print(va_arg(args, unsigned int), 16, "0123456789abcdef");
+			break;
+		case 'X':
+			_putchar('0');
+			_putchar('X');
+			printed_chars += 2;
+			if (length_modifier == 'l')
+				printed_chars += lX_specifier(args);
+			else if (length_modifier == 'h')
+				printed_chars += hX_specifier(args);
+			else
+				printed_chars += convert_and_print(va_arg(args, unsigned int), 16, "0123456789ABCDEF");
+			break;
+		default:
+			printed_chars += process_specifier(specifier, args, length_modifier);
+			break;
 	}
 	return (printed_chars);
 }
